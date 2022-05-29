0835ccd666ce5478d1e1542c4461ac1dc72b13d3
Riy <97500368+riychdwayne@users.noreply.github.com>
2022-05-29 23:18:54 +0700

    ganti apikey

--- settings.js
+++ settings.js
@@ -18,7 +18,7 @@ zenz: 'https://zenzapis.xyz',
 //—————「 Website Apikey 」—————//
 global.APIKeys = {
 //gausah di apa² in, udah free apikey :)
-'https://zenzapis.xyz': 'bagasganz',
+'https://zenzapis.xyz': 'BagasGanz',
 }
 
 //—————「 Setting Owner 」—————//
