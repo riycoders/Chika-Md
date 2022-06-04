c48a0e0e4901fa73c3eb8a275a82e92cf76f8233
ogenkidesuka <106530585+ogenkidesuka@users.noreply.github.com>
2022-06-04 14:14:50 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,12 +24,12 @@ global.APIKeys = {
 //—————「 Setting Owner 」—————//
 global.owner = ['6283894435611','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
 global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6283894435611' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Tn_vams' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'vams' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Tn_vams' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
@@ -41,8 +41,8 @@ global.email = 'riychdwaynee@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = 'vams' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Tn_vams' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
