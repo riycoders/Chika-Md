78a666668bef94bd3852c31ca0cec03b065a3bb0
marsready <109245764+marsready@users.noreply.github.com>
2022-07-14 13:16:33 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-  /*
+/*
   Created By Riy
   Base Ori : DikaArdnt
   Kalau Mau Nambahin Fitur Di › index.js
@@ -19,25 +19,25 @@
   }
 
   // Setting Owner
-  global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.owner = ['62895332008770','62895332008770','62895332008770'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.premium = ['62895332008770'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.ownernomer = '62895332008770' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
   // Set Nama Own & Bot
-  global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-  global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.ownername = 'Mars' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.botname = 'Bunny' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+  global.footer = 'Mars' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Kebutuhan Button & Kontak
   //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
+  global.youtube = 'https://youtube.com/akumars'
   global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-  global.myweb = 'https://api-riychdwayne.herokuapp.com'
+  global.myweb = 'https://www.instagram.com/m_arifin_syam/'
   global.region = 'indonesia'
 
   // Set Wm
-  global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.packname = '© Bunny' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+  global.author = 'Mars' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Nama Session
   //gausah di apa² in!
@@ -66,15 +66,15 @@
   // Message
   //terserah mau ubah apa nggak, note : tanda ' gausah di hapus!
   global.mess = {
-  success: '_Done._',
-  admin: '_Fitur Khusus Admin Group!_',
-  botAdmin: '_Bot Harus Menjadi Admin Terlebih Dahulu!_',
-  owner: '_Fitur Khusus Owner Bot_',
-  group: '_Fitur Digunakan Hanya Untuk Group!_',
-  private: '_Fitur Digunakan Hanya Untuk Private Chat!_',
-  bot: '_Fitur Khusus Pengguna Nomor Bot_',
-  wait: '_Sedang Di Proses..._',
-  error: '_Fitur Sedang Error!_',
+  success: 'Done.',
+  admin: 'Fitur Khusus Admin Group!',
+  botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+  owner: 'Fitur Khusus Owner Bot',
+  group: 'Fitur Digunakan Hanya Untuk Group!',
+  private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+  bot: 'Fitur Khusus Pengguna Nomor Bot',
+  wait: 'Sedang Di Proses...',
+  error: 'Fitur Sedang Error!',
   }
 
   // Batas Akhir
